f04142ddb7737f3059fdf05ad9ba4afd9af520ba
Vasyl Roshko <kotyhoroshko@i.ua>
2017-07-03 15:06:42 +0300

    HW#9 JS

---
+++ hw9/jober.js
@@ -0,0 +1,8 @@
+
+alert (document.getElementById("one").innerHTML);
+
+var span = document.createElement('span');
+span.innerHTML = "Hello world!";    
+document.getElementById("two").insertBefore((span), document.getElementById("two").firstChild);
+
+document.getElementById("ttt").innerHTML = ("PROIZVOLNYI TEXT");
