44f751932f45085620f6137a28f5793f0fe10a3b
Vasyl Roshko <kotyhoroshko@i.ua>
2017-09-01 00:31:39 +0300

    HW#14 JS-6 after mentor coment

--- hw14/js6.js
+++ hw14/js6.js
@@ -1,7 +1,11 @@
 var xCoordinate = 0;
+var msg = 0;
 const trigerItem = document.querySelector('.triger_item');
 	  trigerItem.addEventListener('click', changeLocation );
 
-function changeLocation() {
-	xCoordinate += 38;
-	trigerItem.style.cssText = ("left:"+ ( xCoordinate%(114) ) +"%;"); }
+function changeLocation()
+	{
+	xCoordinate++;
+	trigerItem.style.cssText = ("left:"+ ( (xCoordinate%3)*38 ) +"%;");
+	console.log((xCoordinate%3));
+	}
