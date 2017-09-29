a4f574fbc3df95d01fe2e536105253f38671178d
Vasyl Roshko <kotyhoroshko@i.ua>
2017-09-30 01:34:24 +0300

    HW18-2

--- hw18/js5.js
+++ hw18/js5.js
@@ -11,7 +11,7 @@ function showLS(){
     }
 
     let divRes = document.createElement("div");
-        divRes.style.cssText = "background-color:rgba(0,0,0,.8);color:snow;position:fixed;bottom:10px;left:50%;transform:translate(-50%,0);width:97%;";
+        divRes.style.cssText = "background-color:rgba(0,0,0,.8);color:snow;position:fixed;bottom:10px;left:50%;transform:translate(-50%,0);width:97%;padding:5px";
         divRes.innerHTML = lsListText;
         document.body.append(divRes);
 }
