82fc681c35e51cf5c314d7e8bc1f6b37acceea14
kotyhoroshko <kotyhoroshko@i.ua>
2018-01-23 17:31:00 +0200

    add iklo

---
+++ hw19/iklo.js
@@ -0,0 +1,11 @@
+function findAlive() {
+    var alive = 0;
+    var iklo = 0;
+    for (var i = guiltys.length - 1; i >= 0; i--) {
+        if (iklo == 0) { alive++; }
+        if (iklo > guiltys[i]) {
+            iklo--;
+        } else { iklo = guiltys[i]; }
+    }
+    showAlive(alive);
+}
