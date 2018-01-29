f6bf87ea9edb89827b2e1378ba5448b4dbb41fbe
kotyhoroshko <kotyhoroshko@i.ua>
2018-01-30 00:30:07 +0200

    pseudoServer v0.2

--- ps/ps.js
+++ ps/ps.js
@@ -1,9 +1,21 @@
+'use strict'
+
+var url = 'https://my-json-server.typicode.com/kotyhoroshko/demo/themes';
+var themes = {
+    num: 911,
+    title: "Додана тема POST",
+    hours: 911,
+    deadline: "29.02.2021",
+    text: "-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!-"
+}
 var post = {
     method: 'post',
-    body: 'test=1'
+    headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(themes)
 }
-var url = 'https://my-json-server.typicode.com/kotyhoroshko/demo/themes';
-
 var status = function(response) {
     if (response.status !== 200) {
         return Promise.reject(new Error(response.statusText))
@@ -14,36 +26,51 @@ var json = function(response) {
     return response.json()
 }
 
-fetch(url, post)
-    .then(status)
-    .then(json)
-    .then(function(data) {
-        console.log('data', data)
-    })
-    .catch(function(error) {
-        console.log('error', error)
-    })
+const getBtn = document.querySelector('.getBtn');
+getBtn.addEventListener('click', function() {
+    fetch(url)
+        .then(status)
+        .then(json)
+        .then(function(data) {
+            console.log('GET data:', data);
+            showRez(data);
+        })
+        .catch(function(error) {
+            console.log('error', error)
+        })
+})
+
+const postBtn = document.querySelector('.postBtn');
+postBtn.addEventListener('click', function() {
+    fetch(url, post)
+        .then(status)
+        .then(json)
+        .then(function(data) {
+            console.log('POST data:', data);
+            var prymitka = { 'УВАГА ': 'позицію "ID" додав сервер! (у вихідних даних його не було)' };
+            data = [data, prymitka];
+            showRez(data);
+        })
+        .catch(function(error) {
+            console.log('error', error)
+        })
+})
 
 
+// ==========================SHOW REZULT=================================================== //
 
-// ============================================================================================= //
+var rezultArea = document.querySelector('.showRez');
 
-
-// const url = 'https://my-json-server.typicode.com/kotyhoroshko/demo/themes';
-
-// fetch(url, myInit).then(function(resp) {
-//         alert('status:' + resp.status);
-//         return resp.json();
-//     })
-//     .then(function(data) {
-//         var ttl, txt = [];
-//         for (var i = 0; i < data.length; i++) {
-//             for (ttl in data[i]) {
-//                 txt[i] += ttl + ": " + (data[i])[ttl] + " ";
-//             }
-//         }
-//         alert(txt)
-//     })
-//     // .catch(function() {
-//     //     alert('...А ВОТ ХУЙ ТАМ, ВАСИЛЬКУ!');
-//     // });
+function showRez(data) {
+    var key;
+    var txt = '';
+    var rez = '';
+    for (var i = 0; i < data.length; i++) {
+        for (key in data[i]) {
+            txt += '<p><b>' + key + ': </b>' + (data[i])[key] + '</p>';
+        }
+        rez += '<div>' + txt + '</div>';
+        txt = ''
+    }
+    rezultArea.innerHTML = rez;
+}
