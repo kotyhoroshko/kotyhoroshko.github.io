e4990690aaddc6e69a3ffd2555389e1fed873d9e
kotyhoroshko <kotyhoroshko@i.ua>
2018-09-03 23:01:23 +0300

    add prev - buklet

--- grand/buklet/script.js
+++ grand/buklet/script.js
@@ -5,31 +5,32 @@ var inner = `<div class="header"></div>
 for (let index = 0; index < base.length; index++) {
     inner += `
     <div class="item size${base[index].size}">
-        <div class="item__pic">
-            <img src="img/${base[index].pic}.jpg" alt="">
-        </div>
-        <img src="./img/leaf.png" class="discBg">
-        <span class="item__discount">${base[index].disc}</span>
-        <div class="line"></div>   
-        <div class="item__price">
-            <span class="item__price--n">${base[index].priceN.slice(0, -2)}</span>
-            <div class="cop">
-                <p class="item__price--n-cop">${base[index].priceN.slice(-2)}</p>
-                <div class="cop right">
-                    <p class="item__price--o right">${base[index].priceO.slice(0, -2)}</p>
-                    <span class="item__price--o-cop right">${base[index].priceO.slice(-2)}</span>
-                </div>
-            </div>            
-        </div>
-        <div class="item__desc">
-            <span>${base[index].desc.replace("$","<br>").trim().replace("$","<br>").trim()}</span>
-        </div>
+    <div class="item__pic">
+        <img src="img/${base[index].pic}.jpg" alt="">
     </div>
+
+    <img src="./img/${base[index].color}.png" class="discBg">
+    <span class="item__discount">${base[index].disc}</span>
+    <div class="line"></div>   
+    <div class="item__price">
+        <span class="item__price--n">${base[index].priceN.slice(0, -2)}</span>
+        <div class="cop">
+            <p class="item__price--n-cop">${base[index].priceN.slice(-2)}</p>
+            <div class="cop right">
+                <p class="item__price--o right">${base[index].priceO.slice(0, -2)}</p>
+                <span class="item__price--o-cop right">${base[index].priceO.slice(-2)}</span>
+            </div>
+        </div>            
+    </div>
+    <div class="item__desc">
+        <span>${base[index].desc.replace("$","<br>").trim().replace("$","<br>").trim()}</span>
+    </div>
+</div>
     `
 }
 inner+= `</div>
         <div class="footer">
-            <p>Супермаркет "Гранд" залишає за собою право змінювати ціни в період дії пропозиції та не несе відповідальність за друкарські помилки. Пропозиція діє з 04.08.2018 до 51.08.2018 (або до закінчення товарних залишків) за адресами: м.Виноградів, вул. Станційна, 1б та вул.Копанська, 221. Фото товарів, розміщених у товарних пропозиція, можуть відрізнятися від фото товарів, що беруть участь в акції.</p>
+            <p>Супермаркет "Гранд" залишає за собою право змінювати ціни в період дії пропозиції та не несе відповідальність за друкарські помилки. Пропозиція діє з ${dataVid}.2018 до ${dataDo}.2018 (або до закінчення товарних залишків) за адресами: м.Виноградів, вул. Станційна, 1б та вул.Копанська, 221. Фото товарів, розміщених у товарних пропозиція, можуть відрізнятися від фото товарів, що беруть участь в акції.</p>
         </div>`;
 wrapper.innerHTML = inner;
 
