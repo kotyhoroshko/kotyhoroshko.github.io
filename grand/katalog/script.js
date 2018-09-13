bbc478d1f653ac02349c57b29c25d1a3ff1f9248
kotyhoroshko <kotyhoroshko@i.ua>
2018-09-13 14:25:55 +0300

    add114 to katalog

--- grand/katalog/script.js
+++ grand/katalog/script.js
@@ -1,9 +1,13 @@
 var wrapper = document.querySelector('.wrapper');
 var inner = "";
-for (let index = 1; index <= 74; index++) {
+for (let index = 1; index <= 141; index++) {
     if (index < 10) {
         inner += `
     <div class="item"><img class="item__img" src="jpg/00${index}.jpg"><span class="item__num">${index}</span></div>
+    `
+    } else if (index >= 100) {
+        inner += `
+    <div class="item"><img class="item__img" src="jpg/${index}.jpg"><span class="item__num">${index}</span></div>
     `
     } else {
         inner += `
