3d72550b35fb7ca64dbf851b6ca949b76f13ed62
kotyhoroshko <kotyhoroshko@i.ua>
2018-10-16 14:19:26 +0300

    add some to katalog

--- grand/katalog/script.js
+++ grand/katalog/script.js
@@ -1,6 +1,6 @@
 var wrapper = document.querySelector('.wrapper');
 var inner = "";
-for (let index = 1; index <= 141; index++) {
+for (let index = 1; index <= 153; index++) {
     if (index < 10) {
         inner += `
     <div class="item"><img class="item__img" src="jpg/00${index}.jpg"><span class="item__num">${index}</span></div>
