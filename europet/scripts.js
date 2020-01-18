e5b1f74874c4f90d406aa6159e769771243e97f1
v.roshko <v.roshko@astoundcommerce.com>
2020-01-19 00:35:14 +0200

    europet v5 finish

--- europet/scripts.js
+++ europet/scripts.js
@@ -1,18 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-let uaBlocks = document.querySelectorAll(".ua");
-let ruBlocks = document.querySelectorAll(".ru");
-let langBtn = document.querySelectorAll(".lang");
-    langBtn[1].addEventListener('click', changeLang);
-    langBtn[0].addEventListener('click', changeLang)
+  let uaBlocks = document.querySelectorAll(".ua");
+  let ruBlocks = document.querySelectorAll(".ru");
+  let langBtn = document.querySelectorAll(".lang__item");
+      langBtn[0].addEventListener('click', setUa);
+      langBtn[1].addEventListener('click', setRu);
 
-function changeLang() {
-  for (let index = 0; index < ruBlocks.length; index++) {
-    ruBlocks[index].classList.toggle('hidden')    
+  function setUa() {
+    for (let index = 0; index < uaBlocks.length; index++) {      
+      ruBlocks[index].classList.add('hidden');
+      uaBlocks[index].classList.remove('hidden');
+      localStorage.lang="ua";
+    }
   }
-  for (let index = 0; index < uaBlocks.length; index++) {
-    uaBlocks[index].classList.toggle('hidden')    
+
+  function setRu() {
+    for (let index = 0; index < ruBlocks.length; index++) {
+      ruBlocks[index].classList.remove('hidden');
+      uaBlocks[index].classList.add('hidden');
+      localStorage.lang="ru"
+    }
   }
-}
+
+  (function getPreviousLang() {
+    if(localStorage.lang=="ru") {   
+      setRu();
+    } 
+  })()
  
 });
