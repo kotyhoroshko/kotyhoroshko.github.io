eb48bf81e80504e60f76ecdcdbb02bb404f69fc2
v.roshko <v.roshko@astoundcommerce.com>
2020-05-03 21:02:09 +0300

    add weather

---
+++ weather/scripts.js
@@ -0,0 +1,434 @@
+// const db2 = {
+//     "Headline": {
+//         "EffectiveDate": "2020-05-01T14:00:00+03:00",
+//         "EffectiveEpochDate": 1588330800,
+//         "Severity": 3,
+//         "Text": "Очікуються численні зливи: п’ятниця вдень - неділя вдень",
+//         "Category": "rain",
+//         "EndDate": "2020-05-03T20:00:00+03:00",
+//         "EndEpochDate": 1588525200,
+//         "MobileLink": "http://m.accuweather.com/uk/ua/korolevo/1214319/extended-weather-forecast/1214319?unit=c",
+//         "Link": "http://www.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?unit=c"
+//     },
+//     "DailyForecasts": [
+//         {
+//             "Date": "2020-05-01T07:00:00+03:00",
+//             "EpochDate": 1588305600,
+//             "Temperature": {
+//                 "Minimum": {
+//                     "Value": 9.7,
+//                     "Unit": "C",
+//                     "UnitType": 17
+//                 },
+//                 "Maximum": {
+//                     "Value": 18.3,
+//                     "Unit": "C",
+//                     "UnitType": 17
+//                 }
+//             },
+//             "Day": {
+//                 "Icon": 12,
+//                 "IconPhrase": "Зливи",
+//                 "HasPrecipitation": true,
+//                 "PrecipitationType": "Rain",
+//                 "PrecipitationIntensity": "Light"
+//             },
+//             "Night": {
+//                 "Icon": 39,
+//                 "IconPhrase": "Місцями хмарно зі зливами",
+//                 "HasPrecipitation": true,
+//                 "PrecipitationType": "Rain",
+//                 "PrecipitationIntensity": "Light"
+//             },
+//             "Sources": [
+//                 "AccuWeather"
+//             ],
+//             "MobileLink": "http://m.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=1&unit=c",
+//             "Link": "http://www.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=1&unit=c"
+//         },
+//         {
+//             "Date": "2020-05-02T07:00:00+03:00",
+//             "EpochDate": 1588392000,
+//             "Temperature": {
+//                 "Minimum": {
+//                     "Value": -.9,
+//                     "Unit": "C",
+//                     "UnitType": 17
+//                 },
+//                 "Maximum": {
+//                     "Value": 3.2,
+//                     "Unit": "C",
+//                     "UnitType": 17
+//                 }
+//             },
+//             "Day": {
+//                 "Icon": 12,
+//                 "IconPhrase": "Зливи",
+//                 "HasPrecipitation": true,
+//                 "PrecipitationType": "Rain",
+//                 "PrecipitationIntensity": "Light"
+//             },
+//             "Night": {
+//                 "Icon": 12,
+//                 "IconPhrase": "Зливи",
+//                 "HasPrecipitation": true,
+//                 "PrecipitationType": "Rain",
+//                 "PrecipitationIntensity": "Light"
+//             },
+//             "Sources": [
+//                 "AccuWeather"
+//             ],
+//             "MobileLink": "http://m.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=2&unit=c",
+//             "Link": "http://www.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=2&unit=c"
+//         },
+//         {
+//             "Date": "2020-05-03T07:00:00+03:00",
+//             "EpochDate": 1588478400,
+//             "Temperature": {
+//                 "Minimum": {
+//                     "Value": 4.1,
+//                     "Unit": "C",
+//                     "UnitType": 17
+//                 },
+//                 "Maximum": {
+//                     "Value": 15.8,
+//                     "Unit": "C",
+//                     "UnitType": 17
+//                 }
+//             },
+//             "Day": {
+//                 "Icon": 15,
+//                 "IconPhrase": "Грози",
+//                 "HasPrecipitation": true,
+//                 "PrecipitationType": "Rain",
+//                 "PrecipitationIntensity": "Moderate"
+//             },
+//             "Night": {
+//                 "Icon": 36,
+//                 "IconPhrase": "Змінна хмарність",
+//                 "HasPrecipitation": false
+//             },
+//             "Sources": [
+//                 "AccuWeather"
+//             ],
+//             "MobileLink": "http://m.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=3&unit=c",
+//             "Link": "http://www.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=3&unit=c"
+//         },
+//         {
+//             "Date": "2020-05-04T07:00:00+03:00",
+//             "EpochDate": 1588564800,
+//             "Temperature": {
+//                 "Minimum": {
+//                     "Value": 3.2,
+//                     "Unit": "C",
+//                     "UnitType": 17
+//                 },
+//                 "Maximum": {
+//                     "Value": 16.8,
+//                     "Unit": "C",
+//                     "UnitType": 17
+//                 }
+//             },
+//             "Day": {
+//                 "Icon": 17,
+//                 "IconPhrase": "Місцями сонячно з грозами",
+//                 "HasPrecipitation": true,
+//                 "PrecipitationType": "Rain",
+//                 "PrecipitationIntensity": "Moderate"
+//             },
+//             "Night": {
+//                 "Icon": 35,
+//                 "IconPhrase": "Місцями хмарно",
+//                 "HasPrecipitation": true,
+//                 "PrecipitationType": "Rain",
+//                 "PrecipitationIntensity": "Light"
+//             },
+//             "Sources": [
+//                 "AccuWeather"
+//             ],
+//             "MobileLink": "http://m.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=4&unit=c",
+//             "Link": "http://www.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=4&unit=c"
+//         },
+//         {
+//             "Date": "2020-05-05T07:00:00+03:00",
+//             "EpochDate": 1588651200,
+//             "Temperature": {
+//                 "Minimum": {
+//                     "Value": 4.4,
+//                     "Unit": "C",
+//                     "UnitType": 17
+//                 },
+//                 "Maximum": {
+//                     "Value": 16.4,
+//                     "Unit": "C",
+//                     "UnitType": 17
+//                 }
+//             },
+//             "Day": {
+//                 "Icon": 6,
+//                 "IconPhrase": "Переважно хмарно",
+//                 "HasPrecipitation": true,
+//                 "PrecipitationType": "Rain",
+//                 "PrecipitationIntensity": "Moderate"
+//             },
+//             "Night": {
+//                 "Icon": 18,
+//                 "IconPhrase": "Дощ",
+//                 "HasPrecipitation": true,
+//                 "PrecipitationType": "Rain",
+//                 "PrecipitationIntensity": "Light"
+//             },
+//             "Sources": [
+//                 "AccuWeather"
+//             ],
+//             "MobileLink": "http://m.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=5&unit=c",
+//             "Link": "http://www.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=5&unit=c"
+//         }
+//     ]
+// }
+const db ={
+    "Headline": {
+        "EffectiveDate": "2020-05-01T08:00:00+03:00",
+        "EffectiveEpochDate": 1588309200,
+        "Severity": 3,
+        "Text": "Очікуються численні зливи: п’ятниця вранці - субота вдень",
+        "Category": "rain",
+        "EndDate": "2020-05-02T20:00:00+03:00",
+        "EndEpochDate": 1588438800,
+        "MobileLink": "http://m.accuweather.com/uk/ua/korolevo/1214319/extended-weather-forecast/1214319?unit=c",
+        "Link": "http://www.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?unit=c"
+    },
+    "DailyForecasts": [
+        {
+            "Date": "2020-04-30T07:00:00+03:00",
+            "EpochDate": 1588219200,
+            "Temperature": {
+                "Minimum": {
+                    "Value": 6.6,
+                    "Unit": "C",
+                    "UnitType": 17
+                },
+                "Maximum": {
+                    "Value": 17.1,
+                    "Unit": "C",
+                    "UnitType": 17
+                }
+            },
+            "Day": {
+                "Icon": 12,
+                "IconPhrase": "Зливи",
+                "HasPrecipitation": true,
+                "PrecipitationType": "Rain",
+                "PrecipitationIntensity": "Light"
+            },
+            "Night": {
+                "Icon": 35,
+                "IconPhrase": "Місцями хмарно",
+                "HasPrecipitation": false
+            },
+            "Sources": [
+                "AccuWeather"
+            ],
+            "MobileLink": "http://m.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=1&unit=c",
+            "Link": "http://www.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=1&unit=c"
+        },
+        {
+            "Date": "2020-05-01T07:00:00+03:00",
+            "EpochDate": 1588305600,
+            "Temperature": {
+                "Minimum": {
+                    "Value": 8.7,
+                    "Unit": "C",
+                    "UnitType": 17
+                },
+                "Maximum": {
+                    "Value": 18.3,
+                    "Unit": "C",
+                    "UnitType": 17
+                }
+            },
+            "Day": {
+                "Icon": 12,
+                "IconPhrase": "Зливи",
+                "HasPrecipitation": true,
+                "PrecipitationType": "Rain",
+                "PrecipitationIntensity": "Light"
+            },
+            "Night": {
+                "Icon": 39,
+                "IconPhrase": "Місцями хмарно зі зливами",
+                "HasPrecipitation": true,
+                "PrecipitationType": "Rain",
+                "PrecipitationIntensity": "Light"
+            },
+            "Sources": [
+                "AccuWeather"
+            ],
+            "MobileLink": "http://m.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=2&unit=c",
+            "Link": "http://www.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=2&unit=c"
+        },
+        {
+            "Date": "2020-05-02T07:00:00+03:00",
+            "EpochDate": 1588392000,
+            "Temperature": {
+                "Minimum": {
+                    "Value": 7.6,
+                    "Unit": "C",
+                    "UnitType": 17
+                },
+                "Maximum": {
+                    "Value": 18.6,
+                    "Unit": "C",
+                    "UnitType": 17
+                }
+            },
+            "Day": {
+                "Icon": 12,
+                "IconPhrase": "Зливи",
+                "HasPrecipitation": true,
+                "PrecipitationType": "Rain",
+                "PrecipitationIntensity": "Light"
+            },
+            "Night": {
+                "Icon": 7,
+                "IconPhrase": "Хмарно",
+                "HasPrecipitation": false
+            },
+            "Sources": [
+                "AccuWeather"
+            ],
+            "MobileLink": "http://m.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=3&unit=c",
+            "Link": "http://www.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=3&unit=c"
+        },
+        {
+            "Date": "2020-05-03T07:00:00+03:00",
+            "EpochDate": 1588478400,
+            "Temperature": {
+                "Minimum": {
+                    "Value": 5.3,
+                    "Unit": "C",
+                    "UnitType": 17
+                },
+                "Maximum": {
+                    "Value": 16.1,
+                    "Unit": "C",
+                    "UnitType": 17
+                }
+            },
+            "Day": {
+                "Icon": 15,
+                "IconPhrase": "Грози",
+                "HasPrecipitation": true,
+                "PrecipitationType": "Rain",
+                "PrecipitationIntensity": "Moderate"
+            },
+            "Night": {
+                "Icon": 35,
+                "IconPhrase": "Місцями хмарно",
+                "HasPrecipitation": false
+            },
+            "Sources": [
+                "AccuWeather"
+            ],
+            "MobileLink": "http://m.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=4&unit=c",
+            "Link": "http://www.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=4&unit=c"
+        },
+        {
+            "Date": "2020-05-04T07:00:00+03:00",
+            "EpochDate": 1588564800,
+            "Temperature": {
+                "Minimum": {
+                    "Value": 2.8,
+                    "Unit": "C",
+                    "UnitType": 17
+                },
+                "Maximum": {
+                    "Value": 16.6,
+                    "Unit": "C",
+                    "UnitType": 17
+                }
+            },
+            "Day": {
+                "Icon": 17,
+                "IconPhrase": "Місцями сонячно з грозами",
+                "HasPrecipitation": true,
+                "PrecipitationType": "Rain",
+                "PrecipitationIntensity": "Moderate"
+            },
+            "Night": {
+                "Icon": 34,
+                "IconPhrase": "Переважно ясно",
+                "HasPrecipitation": false
+            },
+            "Sources": [
+                "AccuWeather"
+            ],
+            "MobileLink": "http://m.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=5&unit=c",
+            "Link": "http://www.accuweather.com/uk/ua/korolevo/1214319/daily-weather-forecast/1214319?day=5&unit=c"
+        }
+    ]
+}
+document.addEventListener("DOMContentLoaded", function(){
+
+fetch("http://dataservice.accuweather.com/forecasts/v1/daily/5day/1214319?apikey=0tV5rkAIYVjrGg5OsD0wjbr4iGAMiU9A&language=uk-ua&metric=true")
+    .then(response => response.json())  
+    .then(json => go(json))   
+    .then(response => response ? false : console.log("Weather forecast is succesfully loaded") )
+    .catch(error => console.log(rej))
+})
+
+function go(db2){ 
+
+  function getDate(date) {
+    let clearDate = date.slice(8, 10)+'.'+date.slice(5, 7)+'.'+date.slice(2, 4)
+    return clearDate
+  }
+
+  function getWeatherColor(temp) {
+    let opacity=.66;
+    let color = [(255/60*(temp+20)),(255-temp+20),(255-(255/60*(temp+20)))];
+        color =color.map(Math.round);
+        return `rgba(${color[0]},${color[1]},${color[2]},${opacity})`
+  }
+
+  function showMainInfo(){
+    document.querySelector('.main-info').innerHTML = `
+      <p class="main-info__dates">${getDate(db2.Headline.EffectiveDate)} 
+       - 
+        ${getDate(db2.Headline.EndDate)}</p>
+      <p class="main-info__desc">${db2.Headline.Text}</p>
+      <p class="main-info__category">${db2.Headline.Category}</p>
+    `
+  }
+
+  function showDaysInfo() {
+    inn=''
+    for (let index = 0; index < 5; index++) {   
+      inn+=`
+        <div class="days-info__item days-info__item--${index+1}" 
+          style="background: linear-gradient(TRANSPARENT 20%, rgba(0,0,0,.66) ), linear-gradient(90deg, 
+            ${getWeatherColor(db2.DailyForecasts[index].Temperature.Maximum.Value)}, 
+            ${getWeatherColor(db2.DailyForecasts[index].Temperature.Minimum.Value)});"
+          >
+            <p class="date">${getDate(db2.DailyForecasts[index].Date)}</p>
+            <div class="temperatures">
+              <p class="temperatures__item">${db2.DailyForecasts[index].Temperature.Maximum.Value}&#176;</p>              
+              <p class="temperatures__item">${db2.DailyForecasts[index].Temperature.Minimum.Value}&#176;</p>
+            </div>
+            <div class="day">
+              <img src="img/${db2.DailyForecasts[index].Day.Icon}-s.png" class="icon" />
+              <p class="phrase">${db2.DailyForecasts[index].Day.IconPhrase}</p>
+            </div>
+            <div class="night">
+              <img src="img/${db2.DailyForecasts[index].Night.Icon}-s.png" class="icon" />
+              <p class="phrase">${db2.DailyForecasts[index].Night.IconPhrase}</p>
+            </div>
+        </div>
+      `
+    }
+    document.querySelector('.days-info').innerHTML = inn;
+  }
+  showMainInfo()
+  showDaysInfo()
+};
+
