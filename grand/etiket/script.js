89b754e8ba9dc4dab2a93bf7e393868a96146f47
v.roshko <v.roshko@astoundcommerce.com>
2020-07-15 13:19:03 +0300

    etiker ready v1

--- grand/etiket/script.js
+++ grand/etiket/script.js
@@ -7,11 +7,11 @@ let db = [
         weight: `250г`, 
         art: '48725',
         sklad: `Склад: сало свине`,
-        pozh: `Поживна цінність: Жири 99.7%`,
+        pozh: `Поживна цінність: Жири 99.7%.`,
         kal: `Калорійність  897.61 кКал.`,
         energy: `Енергетична цінність 3755.7 кДж.`,
         termin: `Термін зберігання 30 діб.`,
-        pic:'0'
+        pic:'3'
     },
     {
         title: `Шкварок молотий`,
@@ -22,59 +22,59 @@ let db = [
         kal: `Калорійність  897.61 кКал.`,
         energy: `Енергетична цінність 3755.7 кДж.`,
         termin: `Термін зберігання 30 діб.`,
-        pic:'0'
+        pic:'4'
     },
     {
         title: `Шкварок стакан`,
         weight: `250г`,  
         art: '218',
         sklad: `Склад :сало свине`,
-        pozh: `Поживна цінність: Жири 99.7%,Білки 0.002, Вуглеводи 0.001`,
+        pozh: `Поживна цінність: Жири 99.7%, Білки 0.002, Вуглеводи 0.001.`,
         kal: `Калорійність  897.61 кКал.`,
         energy: `Енергетична цінність 3755.7 кДж.`,
         termin: `Термін зберігання 30 діб.`,
-        pic:'0'
+        pic:'4'
     },
     {
         title: `Смалець стаканчик`,
         weight: `450г`,  
         art: '1698',
         sklad: `Склад: сало свине.`,
-        pozh: `Поживна цінність: Жири 99.7%`,
+        pozh: `Поживна цінність: Жири 99.7%.`,
         kal: `Калорійність  897.61 кКал.`,
         energy: `Енергетична цінність 3755.7 кДж.`,
         termin: `Термін зберігання 30 діб.`,
-        pic:'0'
+        pic:'3'
     },
     {
-        title: `Салат «Морква по-корейськи`,
+        title: `Салат "Морква\nпо-корейськи"`,
         weight: `450г`,
         art: '58',
         sklad: `Склад: Морква, приправа до моркви по-корейськи, олія, оцет.`,
-        pozh: `Поживна цінність в 100г продукту: Білки 1.9,Жири 12.6,Вуглеводи 5.4`,
+        pozh: `Поживна цінність в 100г продукту: Білки 1.9, Жири 12.6, Вуглеводи 5.4.`,
         kal: `Калорійність 141 кКал.`,
-        termin: `Термін зберігання при t = 2+4С до 72 годин.`,
-        pic:'0'
+        termin: `Термін зберігання при температурі від +1°C до +4°С до 72 годин.`,
+        pic:'1'
     },  
     {
-        title: `Салат «Морква по-корейськи`,
+        title: `Салат "Морква\nпо-корейськи"`,
         weight: `800г`,
         art: '42082',
         sklad: `Склад: Морква, приправа до моркви по-корейськи, олія, оцет.`,
-        pozh: `Поживна цінність в 100г продукту: Білки 1.9,Жири 12.6,Вуглеводи 5.4.`,
+        pozh: `Поживна цінність в 100г продукту: Білки 1.9, Жири 12.6, Вуглеводи 5.4.`,
         kal: `Калорійність 141 кКал.`,
-        termin: `Термін зберігання при t = 2+4С до 72 годин.`,
-        pic:'0'
+        termin: `Термін зберігання при температурі від +1°C до +4°С до 72 годин.`,
+        pic:'1'
     },
     {
-        title: `Буряк маринований з хріном`,
+        title: `Буряк маринований\nз хріном`,
         weight: `450г`,
         art: '26534',
         sklad: `Склад: Буряк, хрін, сіль, цукор,олія, оцет.`,
         pozh: `Поживна цінність в 100г продукту: Білки 3.98, Жири 7.98, Вуглеводи 8.92.`,
         kal: `Калорійність 117.72.`,
-        termin: `Термін зберігання при t = 2+4C до 6 годин.`,
-        pic:'0'
+        termin: `Термін зберігання при температурі від +1°C до +4°C до 72 годин.`,
+        pic:'2'
     }
 ]
 
@@ -85,20 +85,41 @@ function go(){
                    
         inner +=`
             <div class="etiket ${db[index].art}">
-                <h2 class="etiket__title">${db[index].title}</h2>
-                <p class="etiket__weight-title">Вага:</p>
-                <p class="etiket__weight">${db[index].weight}</p>
-                <p class="etiket__art">Артикул: ${db[index].art}</p>            
+                <img class="etiket__logo" src="./img/logo.png" alt="">
+                <div class="header">
+                    <h2 class="etiket__title">${db[index].title}</h2>
+                    <br>
+                    <div class="etiket__weight">
+                    <p class="etiket__weight-title">Вага:</p>
+                    <p class="etiket__weight-value">${db[index].weight}</p>
+                    </div>
+                    <img class="etiket__bez-gmo" src="./img/bez-gmo.svg" alt="">
+                    <p class="etiket__art">Артикул: ${db[index].art}</p>
+                </div>
+
+                <div class="footer">
+                    <div class="flex">
+                        <div class="half">
+                            <p class="etiket__data">Дата виготовлення:</p>
+                            <p class="rect">14.06.2020</p>
+                        </div>
+                        <div class="half">    
+                            <p class="etiket__team">Бригадир:</p>
+                            <p class="rect">team Mate</p>
+                        </div>
+                    </div>                    
+                
+                    <div class="ps">
+                        <p class="etiket__termin">${db[index].termin}</p>
+                        <span class="etiket__pozh">${db[index].pozh}</span>
+                        <span class="etiket__kal">${db[index].kal}</span>
+                        ${getEnergy(index)}                
+                        <p class="etiket__adres">Виробник ТзОВ «Піонер» м.Виноградів, вул.Копанська 221.</p>
+                    </div>
+                </div>
+
                 <img class="etiket__img" src="img/${db[index].pic}.jpg">
-                <p class="etiket__pozh">${db[index].pozh}</p>
-                <p class="etiket__kal">${db[index].kal}</p>
-                ${getEnergy(index)}
-                <p class="etiket__termin">${db[index].termin}</p>
-                <p class="etiket__data">Дата виготовлення:</p>
-                <p class="rect">14.06.2020</p>
-                <p class="etiket__team">Бригадир</p>
-                <p class="rect">team Mate</p>
-                <p class="etiket__adres">Виробник ТзОВ «Піонер» м.Виноградів, вул.Копанська 221.
+                
             </div>
         `
 
@@ -106,7 +127,7 @@ function go(){
 
     function getEnergy(index) {
         if (db[index].energy){
-            return `<p class="etiket__energy">${db[index].energy}</p>`
+            return `<span class="etiket__energy">${db[index].energy}</span>`
         }
         return ''  
     }
