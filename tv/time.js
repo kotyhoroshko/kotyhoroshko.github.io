5044a7a5dc38ea981d33d1514c8451d2121048f0
v.roshko <v.roshko@astoundcommerce.com>
2022-02-25 16:53:16 +0200

    tv v4

--- tv/time.js
+++ tv/time.js
@@ -5,31 +5,28 @@ function Time() {
     var hour = date.getHours();
     // Get current minute
     var minute = date.getMinutes();
-    // Get current second
-    var second = date.getSeconds();
     // Variable to store AM / PM
     var period = "";
     // Assigning AM / PM according to the current hour
     if (hour >= 12) {
-    period = "PM";
+        period = "PM";
     } else {
-    period = "AM";
+        period = "AM";
     }
     // Converting the hour in 12-hour format
     if (hour == 0) {
-    hour = 12;
+        hour = 12;
     } else {
-    if (hour > 12) {
-    hour = hour - 12;
-    }
+        if (hour > 12) {
+            hour = hour - 12;
+        }
     }
     // Updating hour, minute, and second
     // if they are less than 10
     hour = update(hour);
     minute = update(minute);
-    second = update(second);
     // Adding time elements to the div
-    document.getElementById("digital-clock").innerText = hour + " : " + minute + " : " + second + " " + period;
+    document.getElementById("digital-clock").innerText = hour + ":" + minute + " " + period;
     // Set Timer to 1 sec (1000 ms)
     setTimeout(Time, 10000);
    }
