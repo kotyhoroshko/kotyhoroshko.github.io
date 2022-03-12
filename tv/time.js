fb34e5a3c2f9ed3cff72a22fb77cc4ced9507cb6
v.roshko <v.roshko@astoundcommerce.com>
2022-03-12 18:17:21 +0200

    add fullScreen to weather3

--- tv/time.js
+++ tv/time.js
@@ -5,29 +5,12 @@ function Time() {
     var hour = date.getHours();
     // Get current minute
     var minute = date.getMinutes();
-    // Variable to store AM / PM
-    var period = "";
-    // Assigning AM / PM according to the current hour
-    if (hour >= 12) {
-        period = "PM";
-    } else {
-        period = "AM";
-    }
-    // Converting the hour in 12-hour format
-    if (hour == 0) {
-        hour = 12;
-    } else {
-        if (hour > 12) {
-            hour = hour - 12;
-        }
-    }
-    // Updating hour, minute, and second
-    // if they are less than 10
+
     hour = update(hour);
     minute = update(minute);
     // Adding time elements to the div
-    document.getElementById("digital-clock").innerText = hour + ":" + minute + " " + period;
-    document.querySelector(".saver-clock").innerText = hour + ":" + minute + " " + period;
+    document.getElementById("digital-clock").innerText = hour + ":" + minute;
+    document.querySelector(".saver-clock").innerText = hour + ":" + minute;
     // Set Timer to 1 sec (1000 ms)
     setTimeout(Time, 10000);
    }
