401ca6a04bcd0ebad5dcc01d8b1525a68a74542d
kotyhoroshko <kotyhoroshko@i.ua>
2023-05-16 12:51:11 +0300

    fix soul kitchen

--- tv/currency.js
+++ tv/currency.js
@@ -1,18 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     function getCurrency() {
-        fetch(`https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5`)
+        let init = {
+            // method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            // mode: '*cors', // no-cors, *cors, same-origin
+            // // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            // // credentials: 'same-origin', // include, *same-origin, omit
+            // headers: {
+            //   'Content-Type': 'application/json'
+            //   // 'Content-Type': 'application/x-www-form-urlencoded',
+            // }
+        }
+        
+        fetch(`https://api.monobank.ua/bank/currency`, init )
         .then(response => response.json())
         .then(json => operateCurrency(json))
         .then(json => console.log("Currency was succesfully loaded"))
         .catch(error => console.log('ERROR. Currency not loaded'))
+        
     }
 
     function operateCurrency(json){
         let currencyContainer = document.querySelector('.currency');
         currencyContainer.innerHTML= `
-            <p><b>${json[0].ccy} </b>${(+json[0].buy).toFixed(2)} | ${(+json[0].sale).toFixed(2)}</p>
-            <p><b>${json[1].ccy} </b>${(+json[1].buy).toFixed(2)} | ${(+json[1].sale).toFixed(2)}</p>
+            <p><b>USD </b>${(+json[0].rateBuy).toFixed(2)} | ${(+json[0].rateSell).toFixed(2)}</p>
+            <p><b>EUR </b>${(+json[1].rateBuy).toFixed(2)} | ${(+json[1].rateSell).toFixed(2)}</p>
         `
     }
 
